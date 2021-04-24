c98b8b65f6be394eab650b9be42d9bc33fb30eeb
anurag <anuragsawle70@gmail.com>
2021-04-24 11:45:45 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request,redirect
+app = Flask(__name__)
+
+
+data = {'1':'bed+OR+beds','2':'icu','3':'oxygen','4':'ventilator+OR+ventilators',
+        '5':'test+OR+tests+OR+testing','6':'remdesivir','7':'favipiravir',
+        '8':'tocilizumab','9':'plasma','10':'food'}
+
+
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        d = request.form['data']
+        q = 'https://twitter.com/search?q=verified+indore+'
+        x = q + data[d] + '+-"needed"+-"required"&f=live'
+        return redirect(x)
+    return render_template("Homepage.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
