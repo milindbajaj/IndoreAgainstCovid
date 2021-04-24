39451c1feef5ba0a2584feabe290323cf90b67bc
anurag <anuragsawle70@gmail.com>
2021-04-24 16:27:27 +0530

    Adding Counter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,12 +10,32 @@ data = {'1': 'bed+OR+beds', '2': 'icu', '3': 'oxygen', '4': 'ventilator+OR+venti
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    conn = sqlite3.connect('static/counter.db')
+    cur = conn.cursor()
+    cur.execute("select * from counter")
+    a = cur.fetchone()
+    a = a[0]
+    b = a + 1
+    update_query = """Update counter set count = ? where count = ?"""
+    data1 = (b, a)
+    cur.execute(update_query, data1)
+    conn.commit()
     if request.method == 'POST':
         d = request.form['data']
         q = 'https://twitter.com/search?q=verified+indore+'
         x = q + data[d] + '+-"needed"+-"required"&f=live'
         return redirect(x)
     return render_template("IndoreAgainstCovid.html")
+
+
+@app.route('/counter')
+def counter():
+    conn = sqlite3.connect('static/counter.db')
+    cur = conn.cursor()
+    cur.execute("select * from counter")
+    a = cur.fetchone()
+    a = a[0]
+    return str(a)
 
 
 if __name__ == '__main__':
