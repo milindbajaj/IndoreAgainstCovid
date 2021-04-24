5629e1d894cff0502c15afec8cfa794c5717235e
anurag <anuragsawle70@gmail.com>
2021-04-24 14:37:00 +0530

    Setting for Homepage

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
-
-data = {'1':'bed+OR+beds','2':'icu','3':'oxygen','4':'ventilator+OR+ventilators',
-        '5':'test+OR+tests+OR+testing','6':'remdesivir','7':'favipiravir',
-        '8':'tocilizumab','9':'plasma','10':'food'}
-
+data = {'1': 'bed+OR+beds', '2': 'icu', '3': 'oxygen', '4': 'ventilator+OR+ventilators',
+        '5': 'test+OR+tests+OR+testing', '6': 'remdesivir', '7': 'favipiravir',
+        '8': 'tocilizumab', '9': 'plasma', '10': 'food'}
 
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         d = request.form['data']
@@ -16,6 +15,7 @@ def index():
         x = q + data[d] + '+-"needed"+-"required"&f=live'
         return redirect(x)
     return render_template("IndoreAgainstCovid.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
